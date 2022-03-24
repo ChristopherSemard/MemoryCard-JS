ac5d43d977bbce1516b56ed79c6dd9abd647f771
ChristopherSemard <christophersemard76@gmail.com>
2022-03-24 10:16:42 +0100

    Fin du memory

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+// Création de la grille
 document.addEventListener("DOMContentLoaded", () => {
     createCards();
-  });
+});
 
+// Check les clics sur les cartes
+document.addEventListener("click", event => {
+    const eventCard = event.target;
+    const eventParent = eventCard.parentElement;
+    console.log(eventCard)
+    console.log(eventParent)
+    if (eventCard.className.includes('card-back') && !eventParent.className.includes('clickedCard')    )  {
+
+        game(eventCard, eventParent);
+    }
+})
+
+// Variable pour compter les cartes validées et la dernière carte retournée
+let countClickedCardsValid = 0;
+let lastClickedCardId;
+let lastClickedCardName;
 
 const createCards = () =>  {
 
@@ -22,8 +39,7 @@ const createCards = () =>  {
     ]
 
     // Random des cartes
-    cardtable.sort((a, b) => 0.5 - Math.random());
-    console.log(cardtable)
+    // cardtable.sort((a, b) => 0.5 - Math.random());
 
     // Affichage des cartes
     cardtable.forEach((item) => {
@@ -31,6 +47,7 @@ const createCards = () =>  {
         let card = document.createElement("div");
         card.classList = "card";
         card.id = item.id;
+        card.setAttribute("name", item.name);
         gridArea.appendChild(card);
 
         // Création de la face de la carte
@@ -44,13 +61,54 @@ const createCards = () =>  {
         back.classList = "card-back";
         card.appendChild(back);
 
-        card.addEventListener("click", () => {
-            back.classList.toggle("clickedCard");
-            card.classList.toggle("clickedCard");
-        })
+
 
     });
 
 
 
 };
+
+
+function  game(eventCard, eventParent)  {
+
+    // Liste des cartes cliquées
+    let listClickedCards = document.getElementsByClassName("clickedCardBack");
+
+    // Check si c'est un nouveau tour alors on retourne la carte
+    if (listClickedCards.length  < countClickedCardsValid + 1) {
+        flipCard(eventCard, eventParent);
+        lastClickedCardId = eventParent.id;
+        lastClickedCardName = eventParent.getAttribute("name");
+    }
+    
+    // Check si c'est la seconde carte cliquée d'un tour, alors on retourne la carte
+    else if (listClickedCards.length === countClickedCardsValid + 1) {
+        flipCard(eventCard, eventParent);
+        
+        // Si la carte est la même on valide les deux
+        if(lastClickedCardName === eventParent.getAttribute("name")){
+            countClickedCardsValid = countClickedCardsValid+2;
+            // Si les 12 cartes ont été retournées on valide la partie
+            if(countClickedCardsValid === 12){
+                let textBox = document.getElementById('texteVictoire');
+                console.log(textBox)
+                textBox.style.display = "block";
+            }
+        }
+        // Sinon on retourne les cartes et on ne les compte pas
+        else if (lastClickedCardName != eventParent.getAttribute("name")){
+            let lastCard = document.getElementById(lastClickedCardId);
+            setTimeout(flipCard, 1000, eventCard, eventParent);
+            setTimeout(flipCard, 1000, lastCard.lastChild, lastCard);
+            
+
+        }
+    }
+};
+
+// Retourner une carte
+function flipCard(eventCard, eventParent) {
+    eventCard.classList.toggle("clickedCardBack");
+    eventParent.classList.toggle("clickedCard");
+}
